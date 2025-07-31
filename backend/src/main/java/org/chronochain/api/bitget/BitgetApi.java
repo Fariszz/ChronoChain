dbf7dbc4e769651d9506b02dfa2e1babbeddba25
Muhammad Faris Hadi Mulyo <kimfais1234@gmail.com>
2025-08-01 04:37:02 +0700

    feat: add BitgetApi interface

---
+++ backend/src/main/java/org/chronochain/api/bitget/BitgetApi.java
@@ -0,0 +1,15 @@
+package org.chronochain.api.bitget;
+
+import org.chronochain.dto.response.ResponseResult;
+import retrofit2.Call;
+import retrofit2.http.*;
+
+import java.util.Map;
+
+public interface BitgetApi {
+    @GET
+    Call<ResponseResult> sendGetRequest(@Url String url, @QueryMap Map<String, String> paramMap);
+
+    @POST
+    Call<ResponseResult> sendPostRequest(@Url String url, @Body Map<String, String> paramMap);
+}
